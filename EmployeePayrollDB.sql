7dffba75c06b6ebf7761881d4a0818b9d56cdbc0
badjateamisha <amishabadjate175@gmail.com>
2022-06-07 19:12:44 +0530

    Using database function SUM, AVG, MIN, MAX, COUNT

--- EmployeePayrollDB.sql
+++ EmployeePayrollDB.sql
@@ -8,3 +8,18 @@ SELECT * from employee_payroll WHERE Start_Date BETWEEN CAST('2022-01-02' AS DAT
 Alter table employee_payroll add Gender varchar(10)
 UPDATE employee_payroll set Gender ='F' where name = 'Amisha' or name ='Disha';
 UPDATE employee_payroll set Gender ='M' where Name='Aarvik';
+
+SELECT SUM(salary) FROM employee_payroll WHERE Gender = 'F' GROUP BY Gender;
+
+SELECT AVG(salary) from employee_payroll
+SELECT AVG(salary) from employee_payroll WHERE Gender = 'F' GROUP BY Gender;
+
+SELECT Min(salary) from employee_payroll
+SELECT Min(salary) from employee_payroll WHERE Gender = 'F' GROUP BY Gender;
+
+SELECT Max(salary) from employee_payroll
+SELECT Max(salary) from employee_payroll WHERE Gender = 'F' GROUP BY Gender;
+
+SELECT Count(Name) from employee_payroll
+SELECT Count(Name) from employee_payroll where Gender ='F' GROUP BY Gender;
+SELECT Count(Name) from employee_payroll where Gender ='M' GROUP BY Gender;
