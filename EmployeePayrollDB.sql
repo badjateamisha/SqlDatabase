ad777cd7ad3f792683919ad22c59b2635bb639f3
badjateamisha <amishabadjate175@gmail.com>
2022-06-07 18:58:55 +0530

    Retrieve data from the table

--- EmployeePayrollDB.sql
+++ EmployeePayrollDB.sql
@@ -2,3 +2,4 @@ create database payroll_service
 use payroll_service
 create table employee_payroll(ID int identity(1,1) primary key,Name varchar(100),Salary int,Start_Date date)
 INSERT into employee_payroll values ('Amisha',100,'2022-01-03'),('Aarvik',200,'2022-01-02'),('Disha',300,'2022-01-01')
+select * from employee_payroll
